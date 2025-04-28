d1e3940bec14bb0eb47104529659809b64e3d272
STANISLAV PRIKHODKO <BusyStas@users.noreply.github.com>
2025-04-28 04:14:02 +0000

    tx

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-# Define the root route to serve the index.html
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/shop')
@@ -12,4 +11,4 @@ def shop():
     return render_template('shop.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True)
