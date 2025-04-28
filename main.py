b6e4eca52aae97071630ffc41169a4273ff123fa
STANISLAV PRIKHODKO <BusyStas@users.noreply.github.com>
2025-04-28 03:47:00 +0000

    shop link

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
